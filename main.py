975c6df35ad4fa071549cd903d1352d7833ef309
alexey <gusschool2@yandex.ru>
2020-04-17 20:48:20 +0300

    3

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-    from flask import Flask, render_template, redirect, abort, request
+from flask import Flask, render_template, redirect, abort, request
 from flask_login import LoginManager, login_user, logout_user, current_user, login_required
 from data.users import User
 from data.doctors import Doctor
